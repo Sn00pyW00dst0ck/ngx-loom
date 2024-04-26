550b59e6c824dfd0837274d674d989a014f3d8ba
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-04-26 10:39:32 -0400

    Node and Edge Defaults. Transfer More Template From A+chiever

--- projects/ngx-loom/src/lib/loom/loom.component.ts
+++ projects/ngx-loom/src/lib/loom/loom.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, computed, input, signal } from "@angular/core";
+import { Component, EventEmitter, HostListener, Output, computed, input, signal } from "@angular/core";
 import { Node, Edge, Matrix } from "../interface/loom.interface";
 import { identity, scale, smoothMatrix, toSVG, transform, translate } from 'transformation-matrix';
 
@@ -8,7 +8,7 @@ import { identity, scale, smoothMatrix, toSVG, transform, translate } from 'tran
  * The loom component.
  */
 @Component({
-    selector: 'graph',
+    selector: 'loom',
     standalone: true,
     imports: [CommonModule],
     templateUrl: './loom.component.html',
@@ -16,9 +16,9 @@ import { identity, scale, smoothMatrix, toSVG, transform, translate } from 'tran
 })
 export class LoomComponent {
     /**
-     * The dimensions for the loom component.
+     * The DOM dimensions for the loom component.
      */
-    public dimensions = input.required<{ w: number, h: number }>();
+    public DOMDimensions = input.required<{ w: number, h: number }>();
 
     /**
      * The nodes for the loom to display.
@@ -43,8 +43,33 @@ export class LoomComponent {
      */
     protected initialized: boolean = false;
 
+    /**
+     * Tracks if the graph is being panned or not. 
+     */
+    protected isPanning: boolean = false;
+
+    /**
+     * The dimensions of the graph within the loom. Necessary because the graph will not be the same size as the DOM container.
+     */
+    private graphDimensions: { w: number, h: number } = { w: 0, h: 0 };
 
 
+    /**
+     * 
+     */
+    @Output() nodeClicked = new EventEmitter<{ event: MouseEvent, node: Node }>();
+    /**
+     * 
+     */
+    @Output() edgeClicked = new EventEmitter<{ event: MouseEvent, edge: Edge }>();
+    /**
+     * 
+     */
+    @Output() graphPanned = new EventEmitter<void>();
+    /**
+     * 
+     */
+    @Output() graphZoomed = new EventEmitter<void>();
 
 
     /**
@@ -53,6 +78,46 @@ export class LoomComponent {
     constructor() {
 
     }
+
+    //#region DOM Interaction
+
+    /**
+     * A function which fires on each mouse movement. Needed to allow panning to occur.
+     *
+     * @param {MouseEvent} $event the event which triggered this function call
+     */
+    @HostListener('document:mousemove', ['$event'])
+    private onMouseMove = ($event: MouseEvent): void => {
+        if (this.isPanning) {
+            // CALL PANNING FUNCTION
+        }
+    }
+
+    /**
+     * A function which fires on each mouse up event. Needed to allow panning to end.
+     *
+     * @param { MouseEvent } $event the event which triggered this function call
+     */
+    @HostListener('document:mouseup', ['$event'])
+    private onMouseUp = ($event: MouseEvent): void => { this.isPanning = false };
+
+    /**
+     * Emits the proper event whenever a node element is clicked.
+     * 
+     * @param { MouseEvent } $event the MouseEvent which triggered the event
+     * @param { Node } node the node which was clicked
+     */
+    protected onNodeClick = ($event: MouseEvent, node: Node): void => this.nodeClicked.emit({ event: $event, node: node });
+
+    /**
+     * Emits the proper event whenever an edge element is clicked. 
+     * 
+     * @param { MouseEvent } $event the MouseEvent which triggered the event
+     * @param { Edge } edge the edge which was clicked
+     */
+    protected onEdgeClick = ($event: MouseEvent, edge: Edge): void => this.edgeClicked.emit({ event: $event, edge: edge });
+
+    //#endregion
 
 
     //#region Panning
